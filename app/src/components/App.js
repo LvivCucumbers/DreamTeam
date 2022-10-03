25c7624a9fa504e97daabe9710d630f9ba3f70dd
YuriiKondak <was.panlak@gmail.com>
2022-10-03 11:47:14 +0300

    add fetch date from server

--- app/src/components/App.js
+++ app/src/components/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [date, setDate] = useState();
+
+    useEffect(() => {
+        fetch("http://localhost:3000/").then((response) => {
+            return response.text();
+        })
+        .then((data) => {
+            setDate(data);
+        });
+    }, [])
+
+    return (
+        <div>
+            <h1>{date}</h1>
+        </div>
+    );
 }
 
 export default App;
